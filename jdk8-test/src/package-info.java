9c1a558b8be11928d2ba2a3deabf8657c112bd0c
Your Name <you@example.com>
2021-02-16 13:23:09 +0800

    Initial commit

---
+++ jdk8-test/src/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @program: untitled
+ * @Date: 2021/2/9 19:53
+ * @Description:
+ */
+//module jdk8-test {
+//
+//    requires jdk9-test;
+//    requires junit;
+//    requires java.net.http;
+//}
---
+++ jdk9-test/src/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @program: untitled
+ * @Date: 2021/2/9 19:53
+ * @Description:
+ */
+//module jdk9-test {
+//
+//    exports com.atguigu.bean;
+//}
